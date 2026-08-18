d98ab17a62bd017546d06cf58a017e8332869835
Alexandru Chitoiu-Marin <alexandru.chitoiu10@gmail.com>
2026-08-18 21:46:46 +0300

    I am just commiting because I am having dinner soon

--- tests/parser/test_parser_util.c
+++ tests/parser/test_parser_util.c
@@ -7,13 +7,14 @@
 #include <string.h>
 #include "parser_util.h"
 
-
+parser_context_s* pctx;
+token_list* tokens;
 
 /*
-Helper function 
+Helper function to free memory for parser context
 */
 static void clean_up(void) {
-
+    destroy_parser_context(&pctx);
 }
 /*
 Helper function 
@@ -34,6 +35,14 @@ static CU_pSuite create_suite(const char* name,  void(*set_up)(),  void(*tear)()
     return suite;
 }
 
+void test_initialize_parser_context(void) {
+    tokens = token_list_initialize();
+    token_list_add(tokens, initialize_token(TOKEN_PIPE, "|", NULL, 1));
+    token_list_add(tokens, initialize_token(TOKEN_PLUS, "+", NULL, 1));
+    pctx = initialize_parser_context(tokens);
+    CU_ASSERT_PTR_NOT_NULL(pctx);
+}
+
 
 
 int main(void) {
@@ -42,7 +51,9 @@ int main(void) {
     if (CU_initialize_registry() != CUE_SUCCESS)
         errx(EXIT_FAILURE, "can't initialize test registry"); 
 
-    
+    /* Initialize_parser_context suite */
+    CU_pSuite initialize_parser_context_suite = create_suite("initialize_parser_context suite", NULL, clean_up);
+    CU_add_test(initialize_parser_context_suite, "initialize parser context", test_initialize_parser_context);
 
     // run the tests
     CU_basic_run_tests();
