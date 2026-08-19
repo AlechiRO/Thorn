e497e42a72e422495d5b16ea33c3f94633d288d7
Alexandru Chitoiu-Marin <alexandru.chitoiu10@gmail.com>
2026-08-19 14:06:16 +0300

    Set up file structure for parser tests

---
+++ tests/parser/test_parser.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+#include <err.h>
+#include <string.h>
+#include "parser_util.h"
+
+parser_context_s* pctx;
+token_list* tokens;
+
+/*
+Helper function to free memory for parser context
+*/
+static void clean_up(void) {
+    destroy_parser_context(&pctx);
+    token_list_destroy(&tokens);
+}
+/*
+Helper function to initialize default token list
+*/
+static void set_up(void) {
+
+}
+
+/* 
+Helper function to create a suite
+@param name Pointer to the name of the suite
+@return CUnit suite object
+*/
+static CU_pSuite create_suite(const char* name,  void(*set_up)(),  void(*tear)()) {
+    CU_pSuite suite = CU_add_suite_with_setup_and_teardown(name, NULL, NULL, set_up, tear); 
+    if (CU_get_error() != CUE_SUCCESS)
+        errx(EXIT_FAILURE, "%s", CU_get_error_msg());
+    return suite;
+}
+
+
+
+
+
+
+int main(void) {
+
+    // initialize registry
+    if (CU_initialize_registry() != CUE_SUCCESS)
+        errx(EXIT_FAILURE, "can't initialize test registry"); 
+
+    
+
+    // run the tests
+    CU_basic_run_tests();
+
+    // record the number of failures
+    int failures = CU_get_number_of_failures();
+
+    // clean the registry
+    CU_cleanup_registry();
+    return failures == 0 ? 0 : 1;
+}
